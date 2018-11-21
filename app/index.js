ddf766f727a224bd6a57eff0b0053f8bf9170f93
thang.le <thang.le@niteco.se>
2018-11-21 18:07:01 +0700

    Add router handler

--- app/index.js
+++ app/index.js
@@ -17,6 +17,8 @@ const server = http.createServer((req, res) => {
     // Get the headers as an object
     const headers = req.headers;
 
+    const queryStringObject = parsedUrl.query;
+
     // Get the payload, if any
     const decoder = new StringDecoder('utf-8');
     let buffer = '';
@@ -26,11 +28,32 @@ const server = http.createServer((req, res) => {
     req.on('end', function(){
         buffer += decoder.end();
 
-        // Send the response
-        res.end("Hello world\n");
+        // Choose teh handler this request should go to.
+        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-        // Log the request path
-        console.log('Request received with payload: ' + buffer);
+        // Construct the data object to send to the handler
+        const data = {
+            trimmedPath,
+            queryStringObject,
+            method,
+            headers,
+            payload: buffer
+        }
+
+        // Route the request to the handler specified in the router
+        chosenHandler(data, function(statusCode, payload) {
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+            payload = typeof(payload) == 'object' ? payload : {};
+            
+            const payloadString = JSON.stringify(payload);
+            
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            
+            // Log the request path
+            console.log('Returnning this response: ', statusCode, payloadString);
+        });
     });
 });
 
@@ -41,9 +64,15 @@ server.listen(3000, () => {
 // Define the handlers
 const handlers = {};
 
-handlers.sample = function(data, callback) {}
+handlers.sample = function(data, callback) {
+    callback(406, {
+        name: 'sample handler'
+    });
+}
 
-handlers.notFound = function(data, callback) {}
+handlers.notFound = function(data, callback) {
+    callback(404);
+}
 
 // Define a request router
 const router = {
